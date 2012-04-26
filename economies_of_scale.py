2310a6f35c68f4c430c2fc0d4e712e9b4360516c
Jonathan Butcher <footley@gmail.com>
2012-04-26 15:05:54 +0200

    also check the b2b and compare "normalized" prices, the proce normalization is naive and simple reduces the price by 1% of price * quality, probably overestimates the impact of quality

--- economies_of_scale.py
+++ economies_of_scale.py
@@ -12,15 +12,17 @@ from bs4 import BeautifulSoup
 CONFIG = 'economies_of_scale.config'
 
 total_qty_re = re.compile("^Total Quantity: ")
-import_href_re = re.compile("^/eos/market-import-cat\.php\?cat=")
 buy_click_re = re.compile("^mB.buyFromMarket\(")
 
 class Web(object):
     def __init__(self, config):
+        self.num_requests = 0
         self.config = config
         self.cookie = cookielib.MozillaCookieJar()
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookie))
         self.load_cookie()
+        
+        self.request_cache = {}
     
     def load_cookie(self):
         try:
@@ -53,70 +55,141 @@ class Web(object):
         else:
             return False
     
-    def read_page(self, url, data=None):
+    def read_page(self, url, data=None, cache=False):
+        url_data = ''
+        if data:
+            url_data = urllib.urlencode(data)
+        
+        if cache:
+            key = url + "$$" + url_data
+            # check cache
+            if key in self.request_cache:
+                return self.request_cache[key]
+        
+        self.num_requests = self.num_requests + 1
         time.sleep(1.0)
         if data:
-            r = self.opener.open(url, urllib.urlencode(data))
+            r = self.opener.open(url, url_data)
         else:
             r = self.opener.open(url)
+        
+        if cache:
+            self.request_cache[key] = r.read()
+            return self.request_cache[key]
+            
         return r.read()
         
-    def get_page_soup(self, url, data=None):
-        source = self.read_page(url, data)
+    def get_page_soup(self, url, data=None, cache=False):
+        source = self.read_page(url, data, cache)
         return BeautifulSoup(source)
+        
+class ProductInfo(object):
+    def __init__(self, quality, qty, price):
+        self.quality = quality
+        self.qty = qty
+        self.price = price
+        self.normalized_price = self.price - ((self.price * 0.01) * self.quality)
+
+def import_create_prod_info(cells):
+    return ProductInfo(
+        float(cells[3].text),
+        float("inf"),
+        float(cells[4].text[1:].replace(',', '')))
+
+def b2b_create_prod_info(cells):
+    return ProductInfo(
+        float(cells[2].text),
+        float(cells[3].text),
+        float(cells[4].text[1:].replace(',', '')))
 
 class ReStocker(object):
     def __init__(self, web, config):
         self.web = web
         self.config = config
         
-    def buy_prod(self, prod_div, prod):
-        # get category id
-        import_anchor = prod_div.find('a', {'href': import_href_re})
-        cat_id = re.sub(import_href_re, '', import_anchor["href"])
-        
-        # loop through cetegory pages until find correct page
-        for page_num in range(1, 16):
-            soup = self.web.get_page_soup(self.config['urls']['import_cat'].format(cat_id, page_num))
-            img = soup.find('img', {'title': prod["name"]})
-            if img:
-                # get the product id
-                prod_row = img.findParent('tr')
-                buy_anchor = prod_row.find('a', {'onclick': buy_click_re})
+    def _get_prod_info(self, store, prod, store_url_key, store_id_key, create_prod_info):
+        prods = []
+        for page in range(1, 16):
+            soup = self.web.get_page_soup(self.config['urls'][store_url_key].format(self.config["store_classes"][store["class"]][store_id_key], page), cache=True)
+            anchor = soup.find('a', text=prod["name"])
+            if not anchor and len(prods) > 0:
+                return sorted(prods, key=lambda p: p.normalized_price)
+            if not anchor:
+                continue
+            row = anchor.findParent('tr')
+            while row:
+                try:
+                    if not row.find('a', text=prod["name"]):
+                        return sorted(prods, key=lambda p: p.normalized_price)
+                except TypeError:
+                    break
+                cells = [cell for cell in row]
+                prod_info = create_prod_info(cells)
+                prods.append(prod_info)
+                buy_anchor = row.find('a', {'onclick': buy_click_re})
                 prod_id = re.sub(buy_click_re, '', buy_anchor["onclick"])
-                index = prod_id.find(',')
-                prod_id = prod_id[0:index]
-                # buy the product
-                self.web.read_page(self.config['urls']['buy_page'].format(prod_id, prod["buy"]))
-                return True
+                prod_info.prod_id = prod_id[0:prod_id.find(',')]
+                row = row.nextSibling
+        return sorted(prods, key=lambda p: p.normalized_price)
         
-        logging.warning("product {0} not found".format(prod["name"]))
-        return False
+    def get_import_prod_info(self, store, prod):
+        return self._get_prod_info(store, prod, 'import_store', 'import_id', import_create_prod_info)
         
-    def process_prod(self, store_soup, prod):
+    def get_b2b_prod_info(self, store, prod):
+        return self._get_prod_info(store, prod, 'b2b_store', 'b2b_id', b2b_create_prod_info)
+        
+    def buy_b2b_prod(self, prod, prod_info):
+        buy_amount = min(prod_info.qty, prod["buy"])
+        logging.info("buying {0} of {1}Q ${2} {3} from b2b".format(
+                buy_amount, 
+                prod_info.quality, 
+                prod_info.price, 
+                prod["name"]))
+        self.web.read_page(self.config['urls']['b2b_buy'].format(prod_info.prod_id, buy_amount))
+    
+    def buy_import_prod(self, prod, prod_info):
+        logging.info("buying {0} of {1}Q ${2} {3} from import".format(
+                prod["buy"], 
+                prod_info.quality, 
+                prod_info.price, 
+                prod["name"]))
+        self.web.read_page(self.config['urls']['import_buy'].format(prod_info.prod_id, prod["buy"]))
+        
+    def buy_prod(self, store, prod):
+        # get b2b prices/qualities
+        b2b_prods = self.get_b2b_prod_info(store, prod)
+        import_prods = self.get_import_prod_info(store, prod)
+        
+        # buy the "best" item available
+        if len(b2b_prods) > 0 and b2b_prods[0].normalized_price < import_prods[0].normalized_price:
+            self.buy_b2b_prod(prod, b2b_prods[0])
+        else:
+            self.buy_import_prod(prod, import_prods[0])
+        
+    def get_prod_qty(self, store_soup, prod):
         img = store_soup.find('img', {'title': prod["name"]})
         if not img:
-            print store_soup.prettify()
             logging.warning("could not find product {0}".format(prod["name"]))
-            return
+            return float("inf")
         prod_div = img.findParent('div', {'class': 'prod_choices_item'})
         out = prod_div.find(text='Out of Stock')
         
         if out:
-            logging.info("    out of {0}, re-stocking {1}".format(prod["name"], prod["buy"]))
-            self.buy_prod(prod_div, prod)
+            return 0
         else:
             qty_anchor = prod_div.find('a', {'title': total_qty_re})    
             qty_str = re.sub(total_qty_re, '', qty_anchor['title']).replace(',', '')
-            
-            if int(qty_str) < int(prod["min"]):
-                logging.info("    insufficient {0}, re-stocking {1}".format(prod["name"], prod["buy"]))
-                self.buy_prod(prod_div, prod)
-            #else:
-            #    logging.info("    sufficient {0}".format(prod["name"]))
+            return int(qty_str)
+        
+    def process_prod(self, store_soup, store, prod):
+        qty = self.get_prod_qty(store_soup, prod)
+        if qty == 0:
+            self.buy_prod(store, prod)
+        elif qty <= int(prod["min"]):
+            self.buy_prod(store, prod)
         
     def process_store(self, store):
-        logging.info("processing {0}: {1}".format(store["company_name"], store["name"]))
+        logging.info("processing {0}: {1}".format(store["company_name"], store["class"]))
         
         # switch to the stores parent company
         data = {
@@ -127,7 +200,7 @@ class ReStocker(object):
         # re-stock the products
         store_soup = self.web.get_page_soup(self.config['urls']['store_inv'].format(store["store_id"]))
         for prod in store["products"]:
-            self.process_prod(store_soup, prod)
+            self.process_prod(store_soup, store, prod)
         
         # then hit the lazy x2 button
         self.web.read_page(self.config['urls']['store_lazyx2'].format(store["store_id"]))
@@ -161,7 +234,7 @@ def main():
     restocker = ReStocker(web, config)
     restocker.go()
     
-    logging.info("re-stocking finished")
+    logging.info("re-stocking finished, with {0} requests".format(web.num_requests))
 
 
 if __name__ == "__main__":
